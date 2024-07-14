7b9f03dc426935abd8865a02e5292d33f0758ecb
ubaid1840 <107393880+ubaid1840@users.noreply.github.com>
2024-07-14 13:27:04 +0500

    Code formatting adjusted

--- src/components/codeMirror.tsx
+++ src/components/codeMirror.tsx
@@ -5,87 +5,95 @@ import { useContext, useEffect } from "react";
 import { ReadmeContext } from "../store/context/ReadmeContext";
 import { DataContext } from "../store/context/DataContext";
 import { MainContext } from "../store/context/MainContext";
+import { darkTheme } from "./ui/themes";
 
 interface CodeEditorProps {
     onChange: (code: string) => void;
-  }
+}
 
-  const MirrorEditor: React.FC<CodeEditorProps> = ({ onChange }) => {
+const MirrorEditor: React.FC<CodeEditorProps> = ({ onChange }) => {
     const { code, setCode } = useStore();
     const readmecontext = useContext(ReadmeContext);
     const datacontext = useContext(DataContext);
     const maincontext = useContext(MainContext);
-  
+
     if (!maincontext) {
-      throw new Error('ReadmeContext not found');
+        throw new Error('ReadmeContext not found');
     }
-  
+
     if (!readmecontext) {
-      throw new Error('ReadmeContext not found');
+        throw new Error('ReadmeContext not found');
     }
-  
+
     if (!datacontext) {
-      throw new Error('ReadmeContext not found');
+        throw new Error('ReadmeContext not found');
     }
-  
-    const {  setReadme } = readmecontext;
-    const {  setData } = datacontext;
+
+    const { setReadme } = readmecontext;
+    const { setData } = datacontext;
     const { state: MainState } = maincontext;
 
-    useEffect(()=>{
+    useEffect(() => {
         handleEditorLoaded()
-    },[])
-  
-      async function handleEditorLoaded() {
-         
-          try {
+    }, [])
+
+    async function handleEditorLoaded() {
+
+        try {
             const response = await fetch('/python-playground/readme.txt');
             const text = await response.text();
             setReadme(text);
-      
-          } catch (error) {
+
+        } catch (error) {
             console.error('Error fetching readme.txt:', error);
-          }
-          try {
+        }
+        try {
             const response = await fetch('/python-playground/data.csv');
             const text = await response.text();
             setData(text)
-      
-          } catch (error) {
-            console.error('Error fetching readme.txt:', error);
-          }
-          setCode(MainState.value.data)
-        }
-  
-  
-      function handleCodeOnChange(e: string | undefined) {
-          if(e){
-              onChange(e);
-          }
-        }
-  
-  
 
-        
-  return <CodeMirror  
-  value={code}
-  onChange={handleCodeOnChange}
-  extensions={[python()]}
-  theme={'dark'}
-  basicSetup={{
-    foldGutter: false,
-    allowMultipleSelections: false,
-    lineNumbers: true,
-    highlightActiveLine: false,
-  }}
-  title="main"
-  style={{
-    outline: "none",
-    fontSize: '16px',
-  }}
-  width="100%"
-  height="100vh"
- />;
+        } catch (error) {
+            console.error('Error fetching readme.txt:', error);
+        }
+        setCode(MainState.value.data)
+    }
+
+
+    function handleCodeOnChange(e: string | undefined) {
+        if (e) {
+            onChange(e);
+        }
+    }
+
+
+
+
+    return (
+     
+            <CodeMirror
+            
+                value={code}
+                onChange={handleCodeOnChange}
+                extensions={[python()]}
+                theme={darkTheme}
+                basicSetup={{
+                    foldGutter: false,
+                    allowMultipleSelections: false,
+                    lineNumbers: true,
+                    highlightActiveLine: false,
+                }}
+                title="main"
+                style={{
+                    outline: "none",
+                    fontSize: '16px',
+                    backgroundColor: '#282C34',
+                    height:'100%'
+                }}
+
+            />
+           
+     
+    );
 };
 
 export default MirrorEditor;
